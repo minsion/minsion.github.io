3379ac96faede68ee045eec73726f28e2d591b46
minsion <15238290258@163.com>
2019-04-21 21:29:17 +0800

    add

---
+++ build/precache-manifest.4b07ea5c29e54b12dae858c1f9511399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "d4d5554d128f13a69217",
+    "url": "/static/js/main.d4d5554d.chunk.js"
+  },
+  {
+    "revision": "8d4f55107502b5d2ffef",
+    "url": "/static/js/2.8d4f5510.chunk.js"
+  },
+  {
+    "revision": "d4d5554d128f13a69217",
+    "url": "/static/css/main.df70a9f5.chunk.css"
+  },
+  {
+    "revision": "8d4f55107502b5d2ffef",
+    "url": "/static/css/2.a1799e03.chunk.css"
+  },
+  {
+    "revision": "030683b36d50e56138d4f7435538de18",
+    "url": "/index.html"
+  }
+];
