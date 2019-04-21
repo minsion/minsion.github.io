ac73aaf93276df9973578f16da4111bf7060c532
minsion <15238290258@163.com>
2019-04-21 13:56:35 +0800

    firstCommit

---
+++ build/precache-manifest.2e6c903193e3eaca2640e3bc248f293b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "4dacfc230f0c8200d5b0",
+    "url": "./static/js/main.fd852b67.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "./static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "4dacfc230f0c8200d5b0",
+    "url": "./static/css/main.5bdd44de.chunk.css"
+  },
+  {
+    "revision": "b602411ed36f180552520fccbd0e9a76",
+    "url": "./index.html"
+  }
+];
